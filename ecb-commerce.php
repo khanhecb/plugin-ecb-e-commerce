7894ced7619636ae0f3cf79682ac7adf0262506a
khanhecb <khanhecb@gmail.com>
2025-04-04 17:48:28 +0700

    Add includes - Core

---
+++ ecb-commerce.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Plugin Name: KhanhECB E-Commerce 
+Description: Plugin E-Commerce for WordPress
+Plugin URI: https://github.com/khanhecb/plugin-ecb-e-commerce
+Version: 1.0
+Author: KhanhECB
+*/
+
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+
