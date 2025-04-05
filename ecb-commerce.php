06f92cc64166658d0e518fccdd285124ff85cb6f
khanhecb <khanhecb@gmail.com>
2025-04-06 05:07:35 +0700

    Add includes - Core

--- ecb-commerce.php
+++ ecb-commerce.php
@@ -12,4 +12,9 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+// Define constants
+define('ECB_ECOMMERCE_VERSION', '1.0');
+define('ECB_ECOMMERCE_DIR', plugin_dir_path(__FILE__));
+define('ECB_ECOMMERCE_URL', plugin_dir_url(__FILE__));
+
 
